488051c743ef47bcaa3b1a47cd6bdc4f592bad0a
VinaLx <vinaleux@gmail.com>
2017-07-16 20:44:26 +0800

    add basic backend sources

---
+++ src/ts/backend/app.ts
@@ -0,0 +1,12 @@
+import * as express from "express"
+import * as path from "path"
+
+const app = express();
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, "../node_modules")));
+app.use(express.static(__dirname));
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`listening on port ${port}`)
+});
